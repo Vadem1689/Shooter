c873fa55864f883a39d7313c1663cc9c6a967703
Vadem <101812203+Vadem1689@users.noreply.github.com>
2024-04-21 22:55:10 +0300

    Task 2

--- Assets/Infima Games/Low Poly Shooter Pack - Free Sample/Code/Cooperate/InteractionWthObjects.cs
+++ Assets/Infima Games/Low Poly Shooter Pack - Free Sample/Code/Cooperate/InteractionWthObjects.cs
@@ -4,56 +4,42 @@ using UnityEngine;
 
 public class InteractionWthObjects : MonoBehaviour
 {
+    [SerializeField] private float _interactDistance;
+
+    private IIntractable nearestIntractable;
 
     private void Update()
     {
-        print("2");
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            GetNearest();
+        }
     }
-    //    [SerializeField] private float _interactDistance;
 
-    //    private IIntractable nearestIntractable;
+    private void GetNearest()
+    {
+        Collider[] colliders = Physics.OverlapSphere
+                (transform.position, _interactDistance);
 
-    //    //void Update()
-    //    //{
-    //    //    print("Работает");
-    //    //    if (Input.GetKeyDown(KeyCode.E))
-    //    //    {
-    //    //        GetNearest();
-    //    //        print("Запущено");
-    //    //    }
-    //    //}
+        IIntractable nowIntractable = null;
+        float nearestDistance = float.MaxValue;
 
-    //    private void Update()
-    //    {
-    //        print("Работает");
-    //        if (Input.GetKeyDown(KeyCode.E))
-    //        {
-    //            GetNearest();
-    //            print("Запущено");
-    //        }
-    //    }
-
-    //    private void GetNearest()
-    //    {
-    //        Collider[] colliders = Physics.OverlapSphere
-    //                (transform.position, _interactDistance);
-
-    //        IIntractable nowIntractable = null;
-    //        float nearestDistance = float.MaxValue;
-
-    //        foreach (Collider collider in colliders)
-    //        {
-    //            if (collider.TryGetComponent<IIntractable>(out nowIntractable))
-    //            {
-    //                float distance = Vector3.Distance(transform.position, collider.transform.position);
-    //                if (distance < nearestDistance)
-    //                {
-    //                    nearestDistance = distance;
-    //                    nearestIntractable = nowIntractable;
-    //                }
-    //            }
-    //        }
-    //        nearestIntractable.Interaction();
-    //        print("Выполн");
-    //    }
+        foreach (Collider collider in colliders)
+        {
+            if (collider.TryGetComponent<IIntractable>(out nowIntractable))
+            {
+                float distance = Vector3.Distance(transform.position, collider.transform.position);
+                if (distance < nearestDistance)
+                {
+                    nearestDistance = distance;
+                    nearestIntractable = nowIntractable;
+                }
+            }
+        }
+        if(nearestIntractable !=null)
+        {
+            nearestIntractable.Interaction();
+        }
+        
+    }
 }
